2107b93a7f864e260d1f9edf00bab72b12603b0c
Raghav JU <juraghav@gmail.com>
2026-09-11 17:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ BOARD_CACHE = "[]"
 CACHE_LOCK = threading.Lock()
 
 USERS = {
-    "juraghav": {"password": "2013*2810@963", "role": "teacher"},
-    "class7E": {"password": "Hrnps@24679", "role": "student"}
+    "juraghav@Digiboardleaning.com": {"password": "2234269580", "role": "teacher"},
+    "socialstudiesclass@Digiboardleaning.com": {"password": "2234269580", "role": "student"}
 }
 
 LOGIN_HTML = """<!DOCTYPE html>
